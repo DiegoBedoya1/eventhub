1a09252b66c5a47e2437851ad263407901b01a5e
“sgmunoz1” <sgmunoz@espol.edu.ec>
2025-12-23 23:22:46 -0500

    rutas finales

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,8 +3,9 @@
 use App\Http\Controllers\Api\EventController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/events', [EventController::class, 'index']);           // Catálogo
-Route::post('/events', [EventController::class, 'store']);          // Crear (Organizador)
-Route::get('/events/{id}', [EventController::class, 'show']);       // Detalle
+Route::get('/events', [EventController::class, 'index']);          
+Route::post('/events', [EventController::class, 'store']);          
+Route::get('/events/{id}', [EventController::class, 'show']);   
 Route::post('/events/{id}/register', [EventController::class, 'register']); // Confirmar Asistencia
 Route::delete('/events/{event_id}/cancel', [EventController::class, 'cancelRegistration']);
+Route::get('/events/{id}/participants', [EventController::class, 'getParticipants']); 
